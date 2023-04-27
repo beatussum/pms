baf943ad70da737362f7e4be7df1205836dd6368
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2023-04-27 11:00:45 +0200

    `arduino::ultrasonic_sensor`: add class
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

---
+++ pmscore/include/arduino/ultrasonic_sensor.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022-2023 Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef PMSCORE_ARDUINO_ULTRASONIC_SENSOR_HPP
+#define PMSCORE_ARDUINO_ULTRASONIC_SENSOR_HPP
+
+#include "arduino/arduino.hpp"
+
+#include <Arduino.h>
+
+namespace pmscore::arduino
+{
+    class ultrasonic_sensor
+    {
+        friend void update_main_ultrasonic_sensor_status();
+    public:
+        static constexpr real speed_of_sound = 3.40e-2;
+    public:
+        explicit ultrasonic_sensor(pin_t __pin_echo, pin_t __pin_trig);
+    public:
+        void start_echoing();
+        void stop_echoing() const { digitalWrite(m_pin_echo, LOW); }
+    public:
+        pin_t get_pin_echo() const noexcept { return m_pin_echo; }
+        void set_pin_echo(pin_t __p) { pinMode(m_pin_echo = __p, OUTPUT); }
+
+        pin_t get_pin_trig() const noexcept { return m_pin_trig; }
+        void set_pin_trig(pin_t __p) { pinMode(m_pin_trig = __p, OUTPUT); }
+
+        real get_distance() const noexcept
+            { return static_cast<real>(m_duration) * (speed_of_sound / 2); }
+
+        real get_duration() const noexcept { return m_duration; }
+        bool is_echoing() const { return (digitalRead(m_pin_echo) == HIGH); }
+    private:
+        pin_t m_pin_echo;
+        pin_t m_pin_trig;
+
+        volatile unsigned long m_duration;
+    };
+
+    inline ultrasonic_sensor* main_ultrasonic_sensor = nullptr;
+
+    void set_main_ultrasonic_sensor(ultrasonic_sensor*);
+    void update_main_ultrasonic_sensor_status();
+}
+
+#endif // PMSCORE_ARDUINO_ULTRASONIC_SENSOR_HPP
---
+++ pmscore/src/arduino/ultrasonic_sensor.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2022-2023 Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include "arduino/ultrasonic_sensor.hpp"
+
+namespace pmscore::arduino
+{
+    ultrasonic_sensor::ultrasonic_sensor(pin_t __pin_echo, pin_t __pin_trig)
+        : m_pin_echo(__pin_echo)
+        , m_pin_trig(__pin_trig)
+    {
+        pinMode(m_pin_echo, OUTPUT);
+        pinMode(m_pin_trig, OUTPUT);
+    }
+
+    void ultrasonic_sensor::start_echoing()
+    {
+        m_duration = 0.;
+
+        digitalWrite(m_pin_echo, HIGH);
+    }
+
+    void set_main_ultrasonic_sensor(ultrasonic_sensor* __us)
+    {
+        if (main_ultrasonic_sensor != nullptr) {
+            detachInterrupt(
+                digitalPinToInterrupt(main_ultrasonic_sensor->get_pin_trig())
+            );
+        }
+
+        main_ultrasonic_sensor = __us;
+
+        attachInterrupt(
+            digitalPinToInterrupt(main_ultrasonic_sensor->get_pin_trig()),
+            &update_main_ultrasonic_sensor_status,
+            CHANGE
+        );
+    }
+
+    void update_main_ultrasonic_sensor_status()
+    {
+        if (main_ultrasonic_sensor->is_echoing()) {
+            if (main_ultrasonic_sensor->m_duration == 0.) {
+                main_ultrasonic_sensor->m_duration = micros();
+            } else {
+                main_ultrasonic_sensor->m_duration =
+                    micros() - main_ultrasonic_sensor->m_duration;
+
+                main_ultrasonic_sensor->stop_echoing();
+            }
+        }
+    }
+}
