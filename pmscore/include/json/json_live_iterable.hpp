bf5a19c4388498f8c81a321d80bf55c40a6459c8
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2023-05-21 18:19:39 +0200

    `pmscore::json::json_live_iterable`: add class
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

---
+++ pmscore/include/json/json_live_iterable.hpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2022-2023 Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef PMSCORE_JSON_JSON_LIVE_ITERABLE_HPP
+#define PMSCORE_JSON_JSON_LIVE_ITERABLE_HPP
+
+#include "json/json_basic_iterable.hpp"
+
+namespace pmscore::json
+{
+    class json_live_iterable : public json_basic_iterable
+    {
+    public:
+        explicit json_live_iterable(
+            json_type __type,
+            char __begin_separator,
+            char __end_separator,
+            Stream* __s
+        )
+            : json_basic_iterable(
+                __type,
+                __begin_separator,
+                __end_separator,
+                0
+            )
+
+            , m_stream(__s)
+            , m_is_ended(false)
+            , m_last_child(nullptr)
+        {}
+
+        virtual ~json_live_iterable() override { delete m_last_child; }
+
+        virtual json_variant* clone() const override
+            { return new json_live_iterable(*this); }
+    public:
+        virtual String serialize() override;
+    public:
+        virtual void push_back(const json_variant& __v) override;
+    public:
+        bool is_ended() const noexcept { return m_is_ended; }
+
+        reference_type get_last_child() noexcept { return *m_last_child; }
+
+        const_reference_type get_last_child() const noexcept
+            { return *m_last_child; }
+    private:
+        Stream* m_stream;
+
+        bool          m_is_ended;
+        json_variant* m_last_child;
+    };
+}
+
+#endif // PMSCORE_JSON_JSON_LIVE_ITERABLE_HPP
